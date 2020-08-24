d060ceab28101b8a99eed1967e047171cf98efdd
Abhishek Singh Tomar <abhishek.tomar_cs19@gla.ac.in>
2020-08-24 22:15:37 +0530

    create getters and setters for fields of Book.java

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -10,4 +10,28 @@ public class Book {
     private String nameOfTheBook;
     private String authorOfTheBook;
     private String isbnNumberOfTheBook;
+
+    public String getNameOfTheBook() {
+        return nameOfTheBook;
+    }
+
+    public void setNameOfTheBook(String nameOfTheBook) {
+        this.nameOfTheBook = nameOfTheBook;
+    }
+
+    public String getAuthorOfTheBook() {
+        return authorOfTheBook;
+    }
+
+    public void setAuthorOfTheBook(String authorOfTheBook) {
+        this.authorOfTheBook = authorOfTheBook;
+    }
+
+    public String getIsbnNumberOfTheBook() {
+        return isbnNumberOfTheBook;
+    }
+
+    public void setIsbnNumberOfTheBook(String isbnNumberOfTheBook) {
+        this.isbnNumberOfTheBook = isbnNumberOfTheBook;
+    }
 }
